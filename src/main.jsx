6478ae28f988565bf6e5227a3579ae43834c7ebb
Frank W <frank.1217.wang@gmail.com>
2026-04-12 16:45:18 -0400

    Use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 // routing
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 // bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );

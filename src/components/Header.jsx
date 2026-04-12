4c0baa86ebc56396750585b6e2c468c21d3db8b4
Frank W <frank.1217.wang@gmail.com>
2026-04-12 14:53:11 -0400

    display current page name in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { LinkContainer } from "react-router-bootstrap";
+import { useLocation } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 
 function getNavElement(name, path) {
@@ -9,10 +10,16 @@ function getNavElement(name, path) {
   );
 }
 
+function getCurrentPageName(pages, pathname) {
+  const page = pages.find((page) => page.path === pathname);
+  return page ? page.name : "";
+}
+
 function Header(props) {
   return (
     <Navbar expand="lg">
-      <Navbar.Brand href="/">Frank W.</Navbar.Brand>
+      <Navbar.Brand href="/">Frank W.</Navbar.Brand>{" "}
+      {getCurrentPageName(props.pages, useLocation().pathname)}
       <Nav className="ms-auto">
         {props.pages.map((page) => getNavElement(page.name, page.path))}
       </Nav>

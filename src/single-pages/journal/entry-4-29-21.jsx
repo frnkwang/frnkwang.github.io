728093390d04d085b514381c8005f719eee9fd6e
Frank W <frank.1217.wang@gmail.com>
2026-04-14 21:46:37 -0400

    journal entry042921 page

---
+++ src/single-pages/journal/entry-4-29-21.jsx
@@ -0,0 +1,39 @@
+function Entry042921() {
+  return (
+    <div>
+      <h5 className="ms-auto">Thursday, 4/29/21</h5>{" "}
+      <h5 className="me-auto">10:03 PM</h5>
+      <p>
+        Everything is all done now! I finished a final on Monday evening and one
+        yesterday evening. Neither of them were too bad, but I’m still a bit
+        worried that I made some dumb mistakes. But I think it’s still possible
+        that I get 100s on both.
+        <br />
+        <br />
+        It’s a bit odd, thinking about it now, that I’ve finished my whole first
+        year of college from home. It didn’t feel much different from high
+        school, and it kinda sucked. I hope it’s all better next semester. Ugh.
+        <br />
+        <br />
+        Anyway, there isn’t too much to write about. Monday through Wednesday
+        was all spent studying. I guess one thing to mention is that Dad and I
+        have regularly been going out at 5 for a short walk, which is nice.
+        Today was spent playing a lot of games. Not really productive at all,
+        and I don’t really have anything else to add, either.
+        <br />
+        <br />I have a few free days before the internship starts. I’ll probably
+        try to relearn some python, practice, compose, play games and sleep. But
+        no stress. I’m totally okay if I don’t get around to doing those things.
+        I mean, I want to do them. But top priority is relaxing a bit more
+        before the internship starts.
+        <br />
+        <br />
+        Okay, that’s all. I still do want to keep my sleep schedule, so I don’t
+        have to change it dramatically for the internship. So I’ll sleep now.
+      </p>
+      Frank Wang
+    </div>
+  );
+}
+
+export default Entry042921;

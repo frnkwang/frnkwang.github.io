e6ccf633aa8336dacdf205ee2203a960e60a233c
Frank W <frank.1217.wang@gmail.com>
2026-05-12 22:29:26 -0400

    Add journal entry 20241112

---
+++ src/pages/journal/Entry20241112.jsx
@@ -0,0 +1,140 @@
+import styles from "./JournalEntry.module.css";
+
+export const DISPLAY_NAME = "Entry: November 12th, 2024";
+export const CREATION_DATE = new Date("2024-11-12");
+
+function Entry20241112() {
+  return (
+    <div>
+      <i>
+        An entry more representative of the ones I've been writing since I
+        started work: a brief summary of work, then more reflective thoughts
+        afterwards.
+      </i>
+      <br />
+      <i>Names and locations have been changed for privacy reasons.</i>
+      <div className="hori">
+        <h2 className={styles.entrydate}>Tuesday, 11/12/24</h2>
+        <h2 className={styles.entrytime}>8:35 PM</h2>
+      </div>
+      <p>
+        I’ve realized my entries are getting dry again lately. Lots of summary,
+        not many thoughts. Part of that is definitely because I haven’t really
+        been in a great mood lately, but I figured I should try to get some more
+        thoughts down. That’s the point of this journal, right?
+        <br />
+        <br />
+        Work has been... embarrassing. Jared and David ended up basically
+        figuring out that bug with disappearing logs for me. It was caused by
+        setting up the log file too early, when the log level wasn’t initialized
+        yet (not my fault for having that uninitialized member variable). But I
+        probably should’ve been able to figure that out on my own. I haven’t
+        really made any other significant progress on anything because of this,
+        either. To be honest, I feel like I haven’t been giving 100% at work
+        lately. I think a lot of it has to do with how tired I’ve been lately,
+        which may be related to how lonely I’ve felt lately since all my friends
+        seem to always be busy. That sounds like an excuse now that I write it
+        down, though... Maybe just getting enough sleep will be enough to fix
+        it. Still, I can’t deny that I felt much more energetic at work back
+        when I had something to look forward to over the weekend......
+        <br />
+        <br />
+        Monday after work was legs at the gym, home around 6:40, dinner, then I
+        walked to the library (alone, depressing) to return a book. I went to
+        bed around 10:30, but I woke up around 1:30 with an awful stomachache. I
+        must’ve eaten something bad at dinner. So I was quite tired today. I
+        considered working from home, but my stomach felt better in the morning,
+        so I went. But the poor sleep must’ve caught up to me, and around 3, I
+        got a pretty bad headache, which is pretty rare for me. So I came home
+        right after work. I wanted to sleep, but I instead practiced violin,
+        since I have to learn that 2nd violin part in just a few days for
+        Friday’s concert. I air-fried some chicken for myself, ate around 7, and
+        that’s about it.
+        <br />
+        <br />
+        So, well, yeah. The past 2 days haven’t been great. In fact, if you read
+        the past few entries, you’ll see that the past few weeks haven’t been
+        great either. I think ever since the 10/21/24 entry, the lights in me
+        and my general energy have dimmed a bit. I’ve even noticed myself
+        writing with more ellipses, or whatever those dot-dot-dots are called,
+        which gives me a much more exhausted, defeated, depressed tone. [...] I
+        still think that the bigger reason for my general depression lately (or
+        at least, what I tell myself is the bigger reason, [...]) is how none of
+        my [work] friends seem to want to do anything with me anymore.
+        Sometimes, some of them don’t even respond to my texts. I know I can’t
+        blame anyone – after all, it’s nobody’s fault in particular – but as a
+        whole, everyone being busy or ignoring me still makes me sad. I’ve
+        basically given up on trying to invite anyone to anything, since they
+        all somehow seem to be booked for every weekend up till the new year.
+        It’s stunning to me that people my age plan so far in advance. I was so
+        used to being the prudent one in school, and now I’m on the other end of
+        the table...... So, well, I’ve mostly been relegated to doing nothing
+        fun after work or over the weekends. I suppose I was busy with
+        [orchestra A] some weekends, but even then, I had the mornings open. And
+        it’s alright doing stuff with high school friends, but... I don’t know,
+        it doesn’t really feel the same. Those people – Jeff, Alice, Andrew –
+        have known me for so long. It’s fun hanging out with them, but not very
+        novel anymore. Honestly, maybe I should just go back to inviting my
+        [work] friends out. Even if it’s unlikely that they’ll come, at least
+        it’s a better shot than not asking them at all. Just kinda stings when
+        they reject me so much......
+        <br />
+        <br />
+        Anyway, this all makes me realize how nice my college friends were. I
+        wrote about this before, but it was so nice to just have people around
+        me all the time that I could talk with. About anything. And almost any
+        time, if I asked. It’s probably because we generally had more time as
+        students. Or, at least, time that didn’t require us to be hyper-focused
+        on something else, time where we could “study” together. Nowadays, I’m
+        working for 8.5-9 hours a day and commuting for 1-1.5 hours, and none of
+        that can be spent like that relaxed “study” time with college friends.
+        And I guess everyone living close by on the same campus helped, too.
+        Though, like I’ve said many times in here, I think my [work] friends
+        would be too tired to hang out even if I lived [closer to work] with
+        them. Anyway, this all makes me more excited for my trip back to Ann
+        Arbor in 1.5 weeks.
+        <br />
+        <br />
+        The other thought I’ve had related to this is, am I an extrovert? If you
+        asked me that before that 10/21/24 entry, I would’ve vehemently answered
+        no. But with how much I’ve been wanting to go out with friends, maybe I
+        am? It’s just, I only want to do it with friends, not strangers. Like
+        going to Cavanaugh’s on Saturday on my own – no shot. Terrible
+        experience. But even if the introvert/extrovert distinction is blurry,
+        maybe I’m more extroverted than I want to admit......
+        <br />
+        <br />
+        Okay, moving onto a few other thoughts.
+        <br />
+        <br />
+        The eczema on my hands flared up Sunday morning again. It’s either due
+        to drinking at dinner with Alice on Saturday, or the fact that I was
+        sitting outside in Rittenhouse Square for an hour, composing, while it
+        was cold and dry. I’m going to hope it’s the latter because I don’t want
+        to give up drinking.
+        <br />
+        <br />
+        After spending a week home alone, I realized that I probably ought to be
+        more independent. I mean, I’m capable of it, as evidenced by last week.
+        [...] Cook for myself more, clean after myself more, go grocery shopping
+        for myself more, etc. It made me feel a bit better about myself when I
+        did everything for myself last week [...], so I want to keep trying to
+        do that. Maybe it’ll help me get out of this depressive slump.
+        <br />
+        <br />
+        Last – I mentioned going out to cafes/library more to help me focus on
+        “work” (ie, composing, reading, etc). I recently remembered there’s that
+        Starbucks [...], probably no more than a mile away, totally walkable.
+        I’m not sure if the vibes are sit-down-and- work, but it might be worth
+        a visit. I think I enjoyed the Center City cafe on Saturday, and this is
+        probably the closest one to home.
+        <br />
+        <br />
+        That’s all. Busy the next few days with [orchestra B].
+      </p>
+      <p className={styles.signature}>Frank Wang</p>
+    </div>
+  );
+}
+
+export default Entry20241112;

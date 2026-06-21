de957aaa0c9ab85e72551b97e9547ea2d750366c
Frank W <frank.1217.wang@gmail.com>
2026-06-21 19:50:36 -0400

    post-grad blog

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,12 +1,22 @@
+import { RecentPagesDisplay } from "../components/RecentPagesDisplay";
+
 export const DISPLAY_NAME = "Blog";
 export const CREATION_DATE = new Date("2026/04/12");
 
 function BlogPage() {
   return (
     <div>
-      <h1>Blog Page</h1>
-      <p>Welcome to the blog page!</p>
-      <br />
+      <p>
+        Random thoughts that I've had. These may be spurious things that come up
+        throughout the day, or more thought-out things organized from multiple
+        journal entries into a larger structure.
+      </p>
+      <RecentPagesDisplay
+        props={{
+          searchDir: "/src/pages/blog",
+          includeSection: false,
+        }}
+      />
     </div>
   );
 }
---
+++ src/pages/blog/PostGradWorkingLife.jsx
@@ -0,0 +1,241 @@
+import img from "../../assets/schuylkill.jpg";
+
+export const DISPLAY_NAME = "On the Post-Grad, Working Life";
+export const DISPLAY_IMAGE = img;
+export const CREATION_DATE = new Date("2026/06/21");
+
+function PostGradWorkingLife() {
+  return (
+    <div>
+      <h2>On the Post-Grad, Working Life</h2>I recently turned 24. That’s 2
+      years out of university, nearly 2 years of full-time work. Work has been
+      fine and all, but it’s really the non-working stuff that took the most for
+      me to get used to, which wasn’t at all what I expected.
+      <h3>The Decay of Free Time and the Resulting Loneliness</h3>
+      I think most people, in their final year or semester of college, are at
+      least somewhat aware that life is going to be different once they’re out
+      of school. No more courses, no more exams (thankfully), much more
+      independence, and getting to live in and explore a new place. For a lot of
+      people, it seems like a very positive thing, and it certainly was for me
+      as well. I very much looked forward to the job offer I had lined up, I
+      looked forward to never having to stress about exams anymore, and I looked
+      forward to meeting new people.
+      <br />
+      <br />
+      But I think one thing that’s less talked-about (or at least, not many of
+      my friends talked about it), is how much less time you have. On paper, it
+      might seem like you have more time during working-life because you’re not
+      constantly thinking about homework and studying for exams. But I’d argue
+      the value of such time - the duration multiplied by how stress-free it is
+      - declines once you start working.
+      <br />
+      <br />
+      In college, especially in-person, making friends is trivially easy. Some
+      might even say that they got more out of making friends at college instead
+      of the actual education. You’re constantly surrounded by like-minded
+      people of the same age, you run into new people all the time at the
+      library or dining halls, and everyone knows similar pop culture as you, so
+      socializing is easy. Even for someone like me, who was always much more
+      introverted and had relatively niche hobbies, I found my group of people
+      at college via orchestra.
+      <br />
+      <br />
+      On top of that, once you do have a group of friends, meeting up with them
+      is incredibly easy. Not only do you all live somewhat close together on
+      campus, but there’s plenty of places to meet up, and more importantly,
+      everyone has the time to meet up. Even if people are busy with homework,
+      you can still meet up and “study” together. So many times in college did I
+      just text friends to ask if they were free, and within 30 minutes, we were
+      either eating together or at the library doing homework together. Even if
+      we’re not actively talking to each other or doing homework for the same
+      class, we still spend time in each others’ company. Light-hearted banter
+      always arises in these cases, and it’s enjoyable nonetheless, regardless
+      of how much work we get done (or don’t).
+      <br />
+      <br />
+      Compare that now to working life. I live alone, get up at 7 AM, work all
+      day without talking to any friends, get off at 5 PM, hit the gym, and get
+      home by 7 PM. After dinner, it’s 8 PM and I have at most 2 hours to do
+      anything before I have to crawl back into bed to ensure I’m not brain-dead
+      for work the next day. Certainly those 2 hours are much more stress-free -
+      I never have to think about any homework or exams like when I was in
+      school - but there’s simply so much less time to do anything. I can read a
+      book for a bit, play some music, or play some games, but I don’t get much
+      opportunity to really get deep into an activity before I need to sleep.
+      <br />
+      <br />
+      Of course, there are alternatives to this. I could go out and get dinner
+      with friends. I could go out to a bar and meet new people. I could go to a
+      concert on a weeknight. But all of these come with the caveat that I still
+      have to find my way home afterwards and make sure I get enough sleep to
+      get up at 7 AM the next day. I still do these things occasionally, but one
+      thing I never realized in college was, working for a whole day makes me
+      tired. So much so that some days, when I get home and finish dinner, those
+      valuable 2 hours seem better spent just going straight to sleep anyway.
+      <br />
+      <br />
+      So what does that all lead to? <br />
+      <br />
+      Loneliness.
+      <br />
+      <br />
+      Of course that’s what happens. With my life so centered around my job, I
+      no longer have time to go out, socialize, and meet friends. Despite my
+      free time being much less stressful, I just have so little of it that it’s
+      often not really worth trying to do anything with friends during it.
+      Especially because I often need the time I get alone to decompress from
+      work.
+      <br />
+      <br />
+      I’ve also noticed that this general exhaustion appears true with many of
+      my friends in the first few months of entering the workforce. Even if I
+      have the energy to go out to try to socialize, often they don’t, and
+      there’s not much socializing for me to do in that case. In a way, this
+      makes me feel better that it’s not specifically a me problem. But of
+      course, it doesn’t solve the problem itself.
+      <br />
+      <br />
+      And the natural result of this is the loss of friends. Of course, distance
+      is a huge factor - once people graduate, they move all over the place for
+      their new jobs - but even for my friends that live decently close, the
+      simple fact that I see them less often means that we drift apart. We can
+      still text and call, but there’s always something missing compared to
+      doing things in person, like when we “studied” together during college or
+      went out to get food during our internships.
+      <br />
+      <br />
+      This loneliness is probably what’s hit me the hardest since graduating. To
+      go from laughing with friends on a daily basis to seeing them about once a
+      month is a difficult jump. Nobody really warned us about this kind of
+      outcome when we were still in college. Perhaps it’s because nobody wanted
+      to talk about such a sad reality, or maybe it’s just because nobody even
+      imagined it being possible. But regardless, this is the state of things
+      post-grad for me.
+      <h3>Coping</h3>
+      Immediately after I started working in 2024, I think I still hadn’t fully
+      understood this change in lifestyle. I was still trying to ask friends to
+      meet up after work and especially on weekends - I was busy every single
+      weekend, doing things in and around the city, for the first 4 months or
+      so. But after a while, things really started slowing down. My friends
+      started rain-checking a lot more frequently - possibly because they were
+      getting tired after work - and suddenly, the loneliness really started to
+      hit.
+      <br />
+      <br />
+      It was around 5 months in when I went back to visit my college campus and
+      see all my college friends again. There, I was reminded of what a vibrant
+      social life I had only 9 months prior. I just ran into people I knew
+      randomly, people were free to meet up on a moment’s notice, and I could
+      just banter and laugh with all my friends. So I regained motivation to
+      re-discover my social life post-work.
+      <br />
+      <br />
+      For the next year or so, I tried a bunch of things to meet more people.
+      First was the orchestras - I joined a number of community orchestras,
+      because I had met the majority of my college friends via an orchestra.
+      While playing music again was fun, I found many of the community
+      orchestras in the area were primarily older people - nothing against them,
+      but it didn’t give the same charm as talking with folks my age.
+      <br />
+      <br />I also tried travelling more to visit my friends. Philly, where I
+      live, is conveniently close to both DC and NY - so I made several weekend
+      trips to both to see friends, as well as back to Ann Arbor a few more
+      times for college friends. This was fun too, but I started to overdo it at
+      some point - I remember I was travelling every single weekend, and I got
+      so exhausted with that (and the mid-week orchestra rehearsals) that I
+      couldn’t focus well at work anymore.
+      <br />
+      <br />
+      Over the summer of 2025, I joined a soccer league, which was fun, but not
+      as much as I had hoped because I was so bad at it. I also tried this app
+      that puts you with strangers for dinner, which was a nice concept, but
+      nothing ever came out of those other than forcing me to go out a few times
+      for dinner.
+      <br />
+      <br />
+      However, things started turning around more when I finally started taking
+      more initiative with the few new, similar-aged people I had met in several
+      orchestras. I started occasionally inviting them to randomly get dinner,
+      or invited them over to play some string quartets for the heck of it. I
+      also tried going to some random community concerts I heard about, met more
+      music-lovers there, and started helping out with setting up some shows. I
+      then found an orchestra that plays the video game music of my childhood,
+      and there were tons of like-minded video game lovers there that I’ve been
+      becoming friends with.
+      <br />
+      <br />
+      On the other hand, I also started thinking of my alone-time a bit
+      differently. I began using the time to go out walking, running, and biking
+      more frequently, and I re-discovered a love that I had for biking. I found
+      my way up and down the Schuylkill River Trail from Gray’s Ferry to as far
+      as Phoenixville. I trained my way to a 22-minute 5K, and in the summer, it
+      was always just nice to go out for a walk to the library.
+      <h3>Resolution, 2 Years Later</h3>
+      It’s now been 2 years since I graduated. I don’t know if I’d say that I’m
+      fully adjusted to this new working life yet, but I’m certainly more used
+      to it now. There’s a few things that I had to wrap my head around to
+      accept it:
+      <br />
+      <br />
+      First, and probably the biggest one, is just lowering my standards. It’s
+      not at all feasible to spend an equal amount of time with friends as you
+      did in college. I saw friends on a daily basis in college; nowadays, I’m
+      lucky if I do something with friends more than once a week. Conceptually,
+      this isn’t really a difficult thing to grasp, but it took me a long time
+      to get my head around it. There’s a difference between knowing something
+      is true and fully internalizing its truthiness. Only after 2 years have I
+      finally begun to accept this reality.
+      <br />
+      <br />
+      The next one that I had to do was taking more active initiative to find
+      things that are enjoyable for me. Of course, I had tried this with my
+      friends immediately after work started, but we burnt out. I tried it with
+      orchestras, but couldn’t find many people my age to socialize with. I
+      tried it with travelling, but I got exhausted quickly. Still, even if they
+      seem like general failures, I can’t say I’ve gotten nothing out of them. I
+      met several new friends from orchestras, and with them, I’ve found many
+      new things to do. The friends I visited while travelling now come to visit
+      me semi-frequently too, and it makes life more enjoyable when I have that
+      to look forward to. Because things are harder to organize post-grad, I’ve
+      found that I just have to try organizing them more to get them to stick.
+      In college, you could just ask to get dinner with a friend day-of, but now
+      we have to plan maybe a week in advance. That’s fine, it’s just a
+      different thing, and I have to take initiative to make it happen.
+      <br />
+      <br />
+      There’s also the re-thinking of personal time. I think college had
+      conditioned my mind to think of alone-time as being antisocial - that is,
+      a negative thing. But after experiencing how exhausting work can be, I
+      realize that time alone is valuable in its own way. The important
+      realization for me was that I should spend it exploring and doing things I
+      like. For me, that turned out to primarily be biking. None of my friends
+      bike with me - for me, it’s a very solo sport - but I find it almost just
+      as enjoyable (and honestly, often much more rewarding) to go for a long
+      ride. This time spent alone isn’t a negative waste of time - it’s
+      something that I need for myself.
+      <br />
+      <br />
+      The last one, though slightly depressing, is the acceptance that getting
+      older just means losing touch with some friends. We can (and probably
+      should) do all we can to mitigate it, of course - texting frequently,
+      calling during long commutes, etc. - but when we get older, we drift apart
+      geographically, and that simply makes it harder to maintain that
+      connection. Even for friends that are geographically close by, their
+      interests may drift as time progresses, and that’s fine too. It’s not like
+      I would hate to see them again, but we simply don’t get as much value
+      spending time with each other anymore. This was something hard for me to
+      accept at first - the thought of losing something that I once deemed so
+      valuable is a crushing one - but with time, I’ve accepted it.
+      <br />
+      <br />
+      That’s where I sit now. I’m more or less content with my post-grad life. I
+      do things with my friends in the area perhaps once or twice a week, and
+      then I’ll visit friends in DC or NY (or they’ll visit me) once every 3
+      months or so. Nowhere near as frequent as when I was in college, but with
+      the other adjustments I’ve made to my life, I’ve found a way to be
+      reasonably happy with this life.
+    </div>
+  );
+}
+
+export default PostGradWorkingLife;

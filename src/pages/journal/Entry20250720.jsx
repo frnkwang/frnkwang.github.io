a6a9397ecf2070c46199ea56e6d0e05f66950e6e
Frank W <frank.1217.wang@gmail.com>
2026-09-20 15:16:52 -0400

    journal entry 20250720

---
+++ src/pages/journal/Entry20250720.jsx
@@ -0,0 +1,145 @@
+import styles from "./JournalEntry.module.css";
+import img from "../../assets/journal.png";
+
+export const DISPLAY_NAME = "Entry: July 20th, 2025";
+export const DISPLAY_IMAGE = img;
+export const CREATION_DATE = new Date("2025/7/20");
+
+function Entry20241112() {
+  return (
+    <div>
+      <i>
+        An entry describing my complaint about wasting time, doomscrolling.
+        These kinds of entries usually help me clear up what I'm thinking and
+        create a soft plan for how to fix my issues.
+      </i>
+      <br />
+      <div className="hori">
+        <h2 className={styles.entrydate}>Sunday, 7/20/25</h2>
+        <h2 className={styles.entrytime}>8:47 PM</h2>
+      </div>
+      <p>
+        I have a few more thoughts to write today, which is why I’m here a bit
+        earlier. Most of these thoughts are about my “productivity”, or lack of
+        it, at home. And how much my attention span has been destroyed by
+        doomscrolling. But normal summary first.
+        <br />
+        <br />
+        Saturday: up around 10:30, hit the gym. [...], then spent pretty much
+        the whole afternoon up here in my room playing video games or
+        doomscrolling, for ~5 hours. Which was awful. I was actually fairly
+        tired too, and I kinda wanted to sleep, but I just… didn’t. I ate dinner
+        around 8 and spent some time browsing trails around here [...] before
+        just going to bed. All in all, a rather awful and wasted day. I know I
+        can’t expect every day to be super productive, but this was just
+        outrageous. Ugh.
+        <br />
+        <br />
+        Today was marginally better. Up around 10, hit the gym, ate, did the
+        laundry, and spent from 2-5 reading my category theory book. I’m trying
+        to go back and read a bit slower to make sure I understand it all. And I
+        was still mildly distracted during it… At 5, I ate, then went to my rec
+        soccer game [...] at 7. Half my team wasn’t there (the pain of being on
+        an all-free-agent team…) so we had some randos fill in from the other
+        team. We still lost 3-1, but I definitely feel like we’re doing better
+        than before. I also feel like I’m dragging the team down, mostly because
+        my ball control is bad. I tried passing people 1-on-1 too much today - I
+        need to find open teammates and pass more. And probably also buy a
+        soccer ball and practice a bit more on my won…… After, I just came home,
+        showered, and that’s it.
+        <br />
+        <br />
+        Ok. That summary was shorter than I expected. Onto more thoughts.
+        <br />
+        <br />
+        The main thing is, my doomscrolling has gotten really excessive lately.
+        By “lately,” I mean the past month or so. I’m not doing it at work, of
+        course, but I often feel like it’s the only thing I do at home, other
+        than working out. Yes, it’s a really nice way for me to turn my brain
+        off, relax, and get some superficial entertainment really quickly. But
+        it only feels nice during it, and I always feel awful after just
+        mindlessly scrolling for hours. Because, well, I have other things I
+        want to do, like reading, composing, coding some side projects,
+        practicing violin, running, biking, practicing soccer, meeting up with
+        friends for food (or music-making). But when doomscrolling is so easy to
+        do compared to all those things, I just resort to that to get any
+        satisfaction. The thing is, I’m sure that all those other activities
+        would give me, proportional to time spent, more enjoyment in the long
+        run than doomscrolling. They just take so much more effort. Not
+        necessarily to even do, but just to start doing, because they’re more of
+        a commitment. So I never do them.
+        <br />
+        <br />
+        The other awful thing about doomscrolling, aside from how dangerously
+        easy it is, is how much it’s destroyed my attention span. This problem
+        is only amplified now that I’m doomscrolling so frequently. But to
+        explain, even though I think this point is pretty self-explanatory,
+        doomscrolling is just, at most, minute-long shorts, one after another. I
+        never have to think about anything for longer than that. In fact, 90% of
+        the time, I don’t even watch the full minute - it’s more like 10-20
+        seconds. So, obviously, my brain is no longer trained to be able to
+        concentrate on one thing for more than 20 seconds, or 30 if I’m lucky.
+        Which obviously makes it harder to do all those activities I listed
+        earlier.
+        <br />
+        <br />
+        To be honest, I can’t pin down an exact reason that it’s gotten this bad
+        lately. It definitely got way worse after my April Ann Arbor trip
+        (4/21/25), but it was still kinda tolerable then. It feels like it just
+        slowly, slowly got worse and worse, and now here I am, having a crisis
+        about it. Perhaps it’s a combination of getting busier at work, getting
+        less sleep (so I’m more frequently too tired to do other stuff), and
+        having less stuff to force me to get out on the weekends, like
+        orchestras. Regardless of the reason, I do think it’s a problem I need
+        to fix, and quite badly. I know people joke about how cooked our
+        generation is (not necessarily because of doomscrolling AI brainrot
+        slop, but the point still stands), and I really want to avoid becoming
+        that stereotype. Not only because it’s a kinda nasty picture to paint,
+        but also just for my own sanity, attention span, and ability to function
+        as a normal human being.
+        <br />
+        <br />
+        So, well, how do I fix this? The obvious answer is “just stop
+        doomscrolling, lol” but, much as I hate to say this, I don’t think it’s
+        that easy - it’s kinda an addiction in a way. One idea I had, which
+        comes from my analysis that my attention span is down the drain now, is
+        meditation, weirdly enough. Thought process is, my brain is so used to
+        getting so much stimulus on-demand and very frequently because of
+        doomscrolling, so I need to re-train my brain (or rather, myself) to be
+        okay with longer periods of no stimulation. This will hopefully allow me
+        to focus when doing those other activities I mentioned, which are often
+        less stimulating than doomscrolling. I’ve tried meditating before, and
+        it’s never been easy for me to just… sit there. But it does sound like
+        something I need. I tried this yesterday and made it 10 minutes before I
+        felt like I was wasting time. But I need to realize, this isn’t wasting
+        time. It’s for my own attention span and my own good. Hopefully, as I
+        continue meditating, I get more comfortable with meditating for longer
+        periods of time.
+        <br />
+        <br />
+        The other idea I had was to explicitly chunk out times on my calendar to
+        do stuff. For example “6-8 today is for reading.” This would ideally
+        give myself something to look forward to at every point throughout the
+        day. Not necessarily “look forward to” as in “be excited about,” but
+        just, to expect something. Then I would have something I’m expecting to
+        do, instead of just feeling like I have zero responsibilities and going
+        to doomscroll again. I tried this today (1-3 is reading, 3-5 is
+        composing, then dinner/soccer, then 8:45-10 is journaling), but of
+        course, the problem is, I’m still not forced to do those activities. In
+        fact, I didn’t today - just read all day instead of composing, which is
+        better than doomscrolling, but it very well could’ve been doomscrolling
+        instead. But then it just comes down to a lack of self-discipline. I
+        suppose that’s the real root cause.
+        <br />
+        <br />
+        We’ll see how those 2 ideas go first, though. I know I’ve definitely had
+        doomscrolling crises like this before, but I never felt like I had a
+        definitive solution for it. They just… eventually went away before.
+        Maybe, hopefully, that’ll happen again?
+      </p>
+      <p className={styles.signature}>Frank Wang</p>
+    </div>
+  );
+}
+
+export default Entry20241112;
